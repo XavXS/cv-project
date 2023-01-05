e6a1b5d42aa0d198d90b8e5083887cba6ba4af4c
xav <xavs421@gmail.com>
2023-01-05 15:32:47 -0700

    Add output styling

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import '../styles/Output.css';
 
 class Output extends React.Component {
   render() {
-    return <div id="output"></div>;
+    return (
+      <div id="output">
+        <div className="output">sdf</div>
+      </div>
+    );
   }
 }
 
